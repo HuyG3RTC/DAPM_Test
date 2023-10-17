7d032f85636b3e7a10897a53f0c66c95e0d03943
HEOCHO <53673151+HuyG3RTC@users.noreply.github.com>
2023-10-17 19:21:17 +0700

    Backup2_P6_19:35

---
+++ WebBanHangDoreStore_2.0_BKDB_2/WebBanHangDoreStore_2.0/WebBanHangDoreStore_2.0/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebBanHangDoreStore_2._0.Models;
+
+namespace WebBanHangDoreStore_2._0.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly DorePhoneContext _context;
+
+        public ProductController(DorePhoneContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Detail(int id)
+        {
+            var product = _context.Products.Include(x => x.Cat).FirstOrDefault(x => x.ProductId == id);
+            if (product == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+    }
+}
